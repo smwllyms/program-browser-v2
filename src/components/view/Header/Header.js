9d79f756f5c154eb68f92d5023f06839958a3ca2
Sam Williams <shwilliams@vt.edu>
2023-12-28 21:59:11 -0500

    added menu to header

--- src/components/view/Header/Header.js
+++ src/components/view/Header/Header.js
@@ -1,19 +1,63 @@
 import "./Header.css"
 
-export default function Header(props)
-{
+import React from "react"
+
+export default function Header(props) {
+    const [showFileSubmenu, setShowFileSubmenu] =  React.useState(false);
+    const [showEditSubmenu, setShowEditSubmenu] =  React.useState(false);
+    const [showPluginSubmenu, setShowPluginSubmenu] =  React.useState(false);
+    const [showGUISubmenu, setShowGUISubmenu] = React.useState(false);
+
     const selected = props.selectedPlugin;
     const name = selected ? selected.name : "None";
+
     return (
-        <div className="header">
+        <header className="header">
             <div style={{float:"left"}}>
-                <a href="#newfx" onClick={props.addNewFXPlugin}>Add FX</a>
-                <a href="#new-generator" onClick={props.addNewGeneratorPlugin}>Add Generator</a>
+                <nav>
+                    <ul>
+                        <li onMouseEnter={() => setShowFileSubmenu(true)} onMouseLeave={() => setShowFileSubmenu(false)}>
+                            File
+                            {showFileSubmenu && (
+                                <ul>
+                                    <li>Dummy Submenu 1</li>
+                                    <li>Dummy Submenu 2</li>
+                                </ul>
+                            )}
+                        </li>
+                        <li onMouseEnter={() => setShowEditSubmenu(true)} onMouseLeave={() => setShowEditSubmenu(false)}>
+                            Edit
+                            {showEditSubmenu && (
+                                <ul>
+                                    <li>Dummy Submenu 1</li>
+                                    <li>Dummy Submenu 2</li>
+                                </ul>
+                            )}
+                        </li>
+                        <li onMouseEnter={() => setShowPluginSubmenu(true)} onMouseLeave={() => setShowPluginSubmenu(false)}>
+                            Plugin
+                            {showPluginSubmenu && (
+                                <ul>
+                                    <li><a href="#newfx" onClick={props.addNewFXPlugin}>Add FX</a></li>
+                                    <li><a href="#new-generator" onClick={props.addNewGeneratorPlugin}>Add Generator</a></li>
+                                </ul>
+                            )}
+                        </li>
+                        <li onMouseEnter={() => setShowGUISubmenu(true)} onMouseLeave={() => setShowGUISubmenu(false)}>
+                            GUI
+                            {showGUISubmenu && (
+                                <ul>
+                                    <li>Dummy Submenu 1</li>
+                                    <li>Dummy Submenu 2</li>
+                                </ul>
+                            )}
+                        </li>
+                    </ul>
+                </nav>
             </div>
             <div style={{float:"right"}}>
                 <span>Currently selected: {name}</span>
             </div>
-            
-        </div>
-    )
+        </header>
+    );
 }
